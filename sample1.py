ccf7acfc9e39c2dc145b79d892487d24b913fb2b
Jayesh Jalindar Vethekar <jvetheka@tibco.com>
2019-08-13 14:54:12 +0530

    file is modified and added another logic in it

--- sample1.py
+++ sample1.py
@@ -1,3 +1,4 @@
-import os
-
-print(os.curdir)
+names={'jayesh','rajesh','rahul','ram'}
+for name in names:
+    print("Name {} is allocated for company ".format(name))
+    print("Welcome to the compnay Mr.{}",format(name))
